766a2c446917bbcc0cebcf13b29d927eb5687b16
panda1990 <psnanda.bit@gmail.com>
2018-11-22 16:20:42 -0800

    31. Next Permutation
    Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
    
    If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
    
    The replacement must be in-place and use only constant extra memory.
    
    Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
    
    1,2,3 → 1,3,2
    3,2,1 → 1,2,3
    1,1,5 → 1,5,1

---
+++ LC_31.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+  
+    void nextPermutation(vector<int>& nums) 
+    {
+      
+      if( nums.empty())
+        return ;
+      
+      int n = nums.size(); 
+      int i;
+      int j;
+      //Starting from end of the list, look for a weakly decresing subsequence
+      //This subsequence is basically the last highest order permutation itself
+      //i.e there are no other "next higher" permutation possible for this seubsequence
+      // e.g 0,1,2,5,3,3,0
+      
+      for(i = n - 1; i >= 0; i --)
+      { 
+        if(nums[i-1] >= nums[i])
+          continue;
+        else
+          break;
+      }
+      
+      //if no such subsequence is found, that means the entire input
+      //is already the last highest permutation. so reverse it
+      if ( i == 0){
+        reverse(nums.begin(), nums.end());
+        return;
+      }
+        
+      //get the pivot point which in our example is "2"
+      int pivot = i - 1;
+      
+      //now compare the pivot with the lowest interger of the 
+      //weakly decreasing subsequence such that there exists a
+      //number which is just slightly higher than the pivot
+      
+      //Note how we search from end so that if there are duplicates, then the
+      //duplicate to the right-most hand side of the subsequence 
+      //has to be swapped with the pivot
+      for(j = n-1 ; j >= i; j--){
+        if(nums[j] > nums[pivot])
+          break;
+      }
+      
+      //jth index is the lowest such value in the subsequence such that
+      //nums[j] > pivot
+      
+      //now swap the pivot with the jth index
+      swap(nums[pivot], nums[j]);
+      
+      //num reverse the subsequence until pivot+1th index
+      reverse(nums.begin() + (pivot + 1), nums.end());
+      
+      return ;
+    }
+};
