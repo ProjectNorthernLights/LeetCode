b28247c2aace1b4ee61cf5c9cff73f5d0c5e1f75
panda1990 <psnanda.bit@gmail.com>
2018-11-25 21:07:21 -0800

    Create graphs.h

---
+++ Graphs/inc/graphs.h
@@ -0,0 +1,38 @@
+/*
+ * list.h
+ *
+ *  Created on: Nov 25, 2018
+ *      Author: pradosh
+ */
+
+#ifndef GRAPHS_H_
+#define GRAPHS_H_
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Graphs{
+private:
+  int V;
+  list<int> *Adj;
+
+public:
+  //constructor
+  Graphs(int V){
+    cout << "Initializing graph with vertices : " << V << endl;
+    this->V = V;
+    Adj = new list<int> [V];
+  }
+
+  void BFS(int source);
+  void DFS(int source, vector<bool> &visited);
+  void addedge(int from, int to);
+};
+
+
+
+#endif /* GRAPHS_H_ */
