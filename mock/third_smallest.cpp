2b26fdd0a4f29d350fbf5b4a13fe6290208281b2
panda1990 <psnanda.bit@gmail.com>
2019-01-24 21:04:17 -0800

    use 3 variables for O(n) - follow up q

--- mock/third_smallest.cpp
+++ mock/third_smallest.cpp
@@ -3,41 +3,71 @@
 #include <algorithm>
 #include <iostream>
 #include <unordered_set>
+#include <climits>
 
 using namespace std;
 
 int thirdSmallest(unordered_set<int> candidates)
 {
-  int cnt = 1;
   if(candidates.size() < 3)
     return 0;
     
-  set<int, greater<int>> ordered_set;
-  for(auto iter = candidates.begin();
-      iter != candidates.end();
-      iter ++)
+  int fSmallest = INT_MAX;
+  int sSmallest = INT_MAX;
+  int tSmallest = INT_MAX;
+  
+  for( auto iter = candidates.begin();
+       iter != candidates.end();
+       iter ++)
   {
-    ordered_set.insert(*iter);
+    if( *iter < fSmallest)
+    {
+      tSmallest = sSmallest;
+      sSmallest = fSmallest;
+      fSmallest = *iter;
+    }
+    else if( *iter < sSmallest)
+    {
+      tSmallest = sSmallest;
+      sSmallest = *iter;
+    }
+    else if( *iter < tSmallest)
+    {
+      tSmallest = *iter;
+    }
   }
   
-  for(auto iter = ordered_set.begin();
-      iter != ordered_set.end();
-      iter++, cnt++)
-  {
-    if(cnt == 3)
-      return *iter;
-  }
+  return tSmallest;
 }
 
 int main()
 {
   
   unordered_set<int> candidates ;
+  
+  /*candidates.insert(10);
+  candidates.insert(8);
+  candidates.insert(11);
+  candidates.insert(9);
+  candidates.insert(15);
+  candidates.insert(7);
+  candidates.insert(6);*/
+  
+  /*candidates.insert(10);
+  candidates.insert(8);
+  candidates.insert(11);
+  candidates.insert(9);*/
+  
+  /*candidates.insert(-10);
+  candidates.insert(8);
+  candidates.insert(11);
+  candidates.insert(9);*/
+
   candidates.insert(4);
-  candidates.insert(1);
-  candidates.insert(3);
-  candidates.insert(2);
   candidates.insert(5);
+  candidates.insert(0);
+  candidates.insert(0);
+
   
   cout << " thirdSmallest is " << thirdSmallest(candidates) << endl;
     return 0;
